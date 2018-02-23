85a0299415e8a431aa37fedd88501388905b3e44
Daniil <daniil@fastmail.fm>
2018-02-23 18:22:45 +0300

    Move general functions from `Hermite3` and `M1Hermite1` objects

--- src/main/scala/piecewise/package.scala
+++ src/main/scala/piecewise/package.scala
@@ -1,4 +1,6 @@
+import com.twitter.algebird.Monoid
 
+import scala.collection.Iterator
 
 /**
   *
@@ -49,5 +51,87 @@ package object piecewise {
     }
   }
 
+  implicit class Itr[A](val iter: Iterator[A]){
+    def process[B](fPad: A, first: (A, A, A) => B)
+                  (casual: (A, A, A) => B)
+                  (last: (A, A, A) => B, lPad: A)(implicit M: Monoid[A]): Iterator[B] = {
+      var isFirst = true
+      var p0: A = M.zero
+      var p1: A = M.zero
+      new Iterator[B] {
+        override def hasNext: Boolean = iter.hasNext
+        override def next(): B = {
+          if (isFirst && iter.hasNext){
+            val f0 = iter.next()
+            if (iter.hasNext) {
+              val f1 = iter.next()
+              isFirst = false
+              p0 = f0
+              p1 = f1
+              first(fPad, f0, f1)
+            }
+            else casual(fPad, f0, lPad)
+          }
+          else if (iter.hasNext) {
+            val p2 = iter.next()
+            val r = casual(p0, p1, p2)
+            p0 = p1
+            p1 = p2
+            r
+          }
+          else {
+            last(p0, p1, lPad)
+          }
+        }
+      }
+    }
+  }
+
+  def deriv(xy1: (Double, Double), xy2: (Double, Double)) =
+    (xy2._2 - xy1._2) / (xy2._1 - xy1._1)
+
+  def centralDeriv(xy1: (Double, Double),
+                   xy12: (Double, Double),
+                   xy2: (Double, Double)) = {
+    deriv(xy1, xy2)
+  }
+
+  def boundDervs(values: List[(Double, Double)]) = {
+    val rightVals = values takeRight 2
+    val der1 = deriv(values.head, values.tail.head)
+    val der2 = deriv(rightVals.head, rightVals.tail.head)
+    (der1, der2)
+  }
+
+  def derivatives(values: Iterator[(Double, Double)]): Iterator[Double] = {
+    values.process((0.0, 0.0),
+      (xy0: (Double, Double), xy1: (Double, Double), xy2: (Double, Double)) => {
+        deriv(xy1, xy2)
+      }
+    )(
+      (xy0: (Double, Double), xy1: (Double, Double), xy2: (Double, Double)) => {
+        centralDeriv(xy0, xy1, xy2)
+      }
+    )(
+      (xy0: (Double, Double), xy1: (Double, Double), xy2: (Double, Double)) => {
+        deriv(xy0, xy1)
+      }, (0.0, 0.0)
+    )
+  }
+
+  def derivatives(values: List[(Double, Double)]): Iterator[Double] = {
+    val onBound = boundDervs(values)
+    onBound._1 :: (values, values drop 2).zipped.map(deriv) :::
+      onBound._2 :: Nil
+    Iterator(onBound._1) ++
+      values.sliding(3).map(list => deriv(list(0), list(2))) ++
+      Iterator(onBound._2)
+  }
+
+  def h(low: Double, upp: Double) = upp - low
+
+  def delta(yLow: Double, yUpp: Double, low: Double, upp: Double) = {
+    (yUpp - yLow) / h(low, upp)
+  }
 
 }
